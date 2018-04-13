63f6051ec9e72d7ea71cb05f7b7786e030c4e79c
EManicaro <enrico.manicaro.a103782@mcast.edu.mt>
2018-04-13 13:31:14 +0200

    Set the menu pages
    Set the menu pages, with the external links ready. Internal links will be done once the pages will be ready

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/start');
+		$this->load->view('Home');
+		$this->load->view('template/end');
 	}
 }
